7a2cc16ed1f771a28c6e5aaf5b501b4ee61ee7c0
Newew44 <142455952+Newew44@users.noreply.github.com>
2023-08-21 21:44:19 -0300

    index.js

---
+++ index.js
@@ -0,0 +1,31 @@
+/*
+*Kauê Prestes Vargas
+*Ratos são show de bola
+*/
+console.log("Olá mundo!");
+//prompt("Digite seu nome");
+
+//Variaveis
+const variavelA = 100;
+const variavelB = variavelA + 50;
+console.log(variavelB); 
+
+let numero = 50;
+console.log(numero);
+numero = 80;
+console.log(numero);
+
+//exercicio
+const nome = "Kauê";
+const idade = 17;
+
+console.log("Olá!", "Meu nome é", nome, "e eu tenho", idade, "anos.");
+
+
+const nomeExercicio = "Kauê";
+const sobrenome = "Prestes";
+const idadeExercicio = 17;
+
+let estudante = true; 
+
+console.log("Olá,", "meu nome é", nomeExercicio, sobrenome, "e tenho", idadeExercicio, "anos.", "Sou um estudante do SENAC?", estudante);
